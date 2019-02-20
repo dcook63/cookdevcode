2940edc094b63a6d109ae9a31d1a179b863cd2a1
Alaina Hammonds <alaina.leeann35@gmail.com>
2019-02-20 11:57:12 -0500

    And now putting it all back

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about-alaina', component: HomeComponent },
+  { path: 'about-daniel', component: HomeComponent }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+    ),
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
