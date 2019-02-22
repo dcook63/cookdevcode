d70d2fd93f80abf9854aa8111039ec6da7e5193b
Alaina Hammonds <alaina.leeann35@gmail.com>
2019-02-21 23:21:16 -0500

    Added mobile nav menu

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'cookdev2'`, async(() => {
+  it(`should have as title 'Cook Development'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cookdev2');
+    expect(app.title).toEqual('Cook Development');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cookdev2!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Cook Development!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
+export interface IMenuItem {
+  iconText: string;
+  label: string;
+  route: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'cookdevcode';
+  isMobile = false;
+  slideDownNav = false;
+
+  menuItems: IMenuItem[] = [
+    {
+      iconText: 'fa fa-home',
+      label: ' Home',
+      route: ''
+    },
+    {
+      iconText: 'fa fa-male',
+      label: ' About Daniel',
+      route: 'about-daniel'
+    },
+    {
+      iconText: 'fa fa-female',
+      label: ' About Alaina',
+      route: 'about-alaina'
+    },
+  ];
+
+  ngOnInit() {
+    this.setIsMobile();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.setIsMobile();
+  }
+
+  setIsMobile() {
+    this.isMobile = window.innerWidth <= 768;
+  }
+
+  showSlideDownNav() {
+    this.slideDownNav = !this.slideDownNav;
+  }
 }
