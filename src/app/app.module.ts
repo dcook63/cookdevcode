db3bb9a37d61a7b7726624583fb0d19e45a5afb1
dcook63 <dco769@gmail.com>
2019-05-03 21:08:38 -0400

    Adding new pages and more styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
+  { path: '', redirectTo: "/home", pathMatch: "full"},
   { path: 'home', component: HomeComponent },
   { path: 'about-cookdev', component: AboutCookDevComponent },
   { path: 'alaina/about-alaina', component: AboutAlainaComponent },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  isMobile = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.setIsMobile();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.setIsMobile();
+  }
+
+  setIsMobile() {
+    this.isMobile = window.innerWidth <= 768;
+  }
 }
